2fe6b1877946b89ce0474e800de0e99a7f2ec48f
David <dawid.prosba@gmail.com>
2021-04-25 18:05:58 +0200

    Remove temporary component

--- src/components/pages/home/components/Workspaces.tsx
+++ src/components/pages/home/components/Workspaces.tsx
@@ -51,30 +51,5 @@ const Workspaces: FC = () => (
     </Container>
 )
 
-const Placeholder = () => {
-    return (
-        <Card>
-            <CardActionArea>
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="h2">
-                        Lizard
-                    </Typography>
-                    <Typography variant="body2" color="textSecondary" component="p">
-                        {Math.random()}
-                        Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging
-                        across all continents except Antarctica
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-            <CardActions>
-                <Button size="small" color="primary">
-                    Share
-                </Button>
-                <Button size="small" color="primary">
-                    Learn More
-                </Button>
-            </CardActions>
-        </Card>)
-}
 
 export default Workspaces;
